b3ed8ea8090d878455b842a6fc880f2a23f0130f
parhan <apriliandi642@gmail.com>
2020-11-01 17:58:58 +0700

    add functionality

--- extension.js
+++ extension.js
@@ -1,37 +1,61 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const vscode = require("vscode");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
+
 function activate(context) {
+   const editor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "css" is now active!');
+   if (!editor) {
+      vscode.window.showInformationMessage("Editor does not exist");
+      return;
+   }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('css.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+   if (editor.document.getText(editor.selection) === "".trim()) {
+      vscode.window.showInformationMessage("No Properties Selected");
+      return;
+   }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sort-it!');
-	});
+   const minSort = vscode.commands.registerCommand("sort-it.min", () => {
+      editor.edit((builder) => {
+         builder.replace(editor.selection, onSort("min"));
+      });
+   });
 
-	context.subscriptions.push(disposable);
+   const maxSort = vscode.commands.registerCommand("sort-it.max", () => {
+      editor.edit((builder) => {
+         builder.replace(editor.selection, onSort("max"));
+      });
+   });
+
+   function onSort(format) {
+      let selectionText = editor.document.getText(editor.selection);
+
+      let result = selectionText
+         .split(";")
+         .join("\n")
+         .split("\n")
+         .filter((property) => property.trim() !== "")
+         .map((property) => property + ";")
+         .sort((a, b) =>
+            format === "min"
+               ? a.trim().length - b.trim().length
+               : b.trim().length - a.trim().length
+         )
+         .join("\n");
+
+      return result;
+   }
+
+   context.subscriptions.push(minSort, maxSort);
 }
+
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+   activate,
+   deactivate,
+};
