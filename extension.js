350d84f666959b0db70a0555cdcad67cbc82af72
parhan <apriliandi642@gmail.com>
2020-12-19 15:12:10 +0700

    refactor code

--- extension.js
+++ extension.js
@@ -19,20 +19,21 @@ function activate(context) {
 
       if (cssValidator.validate(`.element {${cssProperties}}`).length > 0) {
          vscode.window.showErrorMessage("Css code is invalid....");
-      } else {
-         const result = cssProperties
-            .split(";")
-            .join("\n")
-            .split("\n")
-            .filter((property) => property.trim() !== "")
-            .map((property) => property + ";")
-            .sort((a, b) => a.trim().length - b.trim().length)
-            .join("\n");
-
-         editor.edit((builder) => {
-            builder.replace(editor.selection, result);
-         });
+         return;
       }
+
+      const result = cssProperties
+         .split(";")
+         .join("\n")
+         .split("\n")
+         .filter((property) => property.trim() !== "")
+         .map((property) => property + ";")
+         .sort((a, b) => a.trim().length - b.trim().length)
+         .join("\n");
+
+      editor.edit((builder) => {
+         builder.replace(editor.selection, result);
+      });
    });
 
    const maxSort = vscode.commands.registerCommand("sort-it.max", () => {
@@ -51,20 +52,21 @@ function activate(context) {
 
       if (cssValidator.validate(`.element {${cssProperties}}`).length > 0) {
          vscode.window.showErrorMessage("Css code is invalid....");
-      } else {
-         const result = cssProperties
-            .split(";")
-            .join("\n")
-            .split("\n")
-            .filter((property) => property.trim() !== "")
-            .map((property) => property + ";")
-            .sort((a, b) => b.trim().length - a.trim().length)
-            .join("\n");
-
-         editor.edit((builder) => {
-            builder.replace(editor.selection, result);
-         });
+         return;
       }
+
+      const result = cssProperties
+         .split(";")
+         .join("\n")
+         .split("\n")
+         .filter((property) => property.trim() !== "")
+         .map((property) => property + ";")
+         .sort((a, b) => b.trim().length - a.trim().length)
+         .join("\n");
+
+      editor.edit((builder) => {
+         builder.replace(editor.selection, result);
+      });
    });
 
    context.subscriptions.push(minSort);
