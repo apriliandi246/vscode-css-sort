47fdd1afe3e5b8220dfc8c7722a2c59fec4ac3fa
Farhan Apriliandi <apriliandi642@gmail.com>
2021-03-24 12:51:00 +0700

    update functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,8 @@
 import * as vscode from "vscode";
 import { parse, generate } from "css-tree";
-// @ts-ignore
-import * as cssValidator from "csstree-validator";
 
 export function activate(context: vscode.ExtensionContext) {
    const minSort = vscode.commands.registerCommand("css-sort.min", () => {
-      let anotherErrors: string = "";
       const editor = vscode.window.activeTextEditor;
       const cssCode = editor?.document.getText(editor.selection).trim();
 
@@ -16,38 +13,6 @@ export function activate(context: vscode.ExtensionContext) {
 
       if (!cssCode) {
          vscode.window.showWarningMessage("No CSS code selected");
-         return;
-      }
-
-      parse(cssCode, {
-         onParseError(error) {
-            if (error.message === "Selector is expected") {
-               anotherErrors = "Selector is expected";
-            }
-
-            if (error.message === "Identifier is expected") {
-               anotherErrors = "Identifier is expected";
-            }
-         },
-      });
-
-      if (anotherErrors !== "") {
-         vscode.window.showWarningMessage(anotherErrors);
-         return;
-      }
-
-      if (cssCode[cssCode.length - 1] !== "}") {
-         vscode.window.showWarningMessage("Right curly bracket is expected");
-         return;
-      }
-
-      if (cssValidator.validate(cssCode).length > 0) {
-         vscode.window.showWarningMessage(
-            cssValidator.validate(cssCode)[0].message ===
-               "LeftCurlyBracket is expected"
-               ? "Left curly bracket is expected"
-               : cssValidator.validate(cssCode)[0].message
-         );
          return;
       }
 
@@ -59,7 +24,6 @@ export function activate(context: vscode.ExtensionContext) {
    });
 
    const maxSort = vscode.commands.registerCommand("css-sort.max", () => {
-      let anotherErrors: string = "";
       const editor = vscode.window.activeTextEditor;
       const cssCode = editor?.document.getText(editor.selection).trim();
 
@@ -70,38 +34,6 @@ export function activate(context: vscode.ExtensionContext) {
 
       if (!cssCode) {
          vscode.window.showWarningMessage("No CSS code selected");
-         return;
-      }
-
-      parse(cssCode, {
-         onParseError(error) {
-            if (error.message === "Selector is expected") {
-               anotherErrors = "Selector is expected";
-            }
-
-            if (error.message === "Identifier is expected") {
-               anotherErrors = "Identifier is expected";
-            }
-         },
-      });
-
-      if (anotherErrors !== "") {
-         vscode.window.showWarningMessage(anotherErrors);
-         return;
-      }
-
-      if (cssCode[cssCode.length - 1] !== "}") {
-         vscode.window.showWarningMessage("Right curly bracket is expected");
-         return;
-      }
-
-      if (cssValidator.validate(cssCode).length > 0) {
-         vscode.window.showWarningMessage(
-            cssValidator.validate(cssCode)[0].message ===
-               "LeftCurlyBracket is expected"
-               ? "Left curly bracket is expected"
-               : cssValidator.validate(cssCode)[0].message
-         );
          return;
       }
 
@@ -120,25 +52,41 @@ function onSort(format: string, cssCode: string): string {
    const cssAst = parse(cssCode, { parseValue: false });
    const arrCssProperties: string[] = generate(cssAst).split("}");
 
-   arrCssProperties.pop();
-
-   for (let index = 0; index < arrCssProperties.length; index++) {
-      const cssNode: string[] = arrCssProperties[index].split("{");
-      const cssSelector: string = cssNode[0].trim();
-
-      const cssProperties: string = cssNode[1]
+   // single sort (just css properties without css selector)
+   if (arrCssProperties[arrCssProperties.length - 1] !== "") {
+      sortedCssProperties = cssCode
          .trim()
-         .split(";")
+         .split("\n")
          .filter((property) => property.trim() !== "")
          .map((property) => property.trim() + ";")
          .sort((a, b) =>
             format === "min" ? a.length - b.length : b.length - a.length
          )
          .join("\n");
+   }
 
-      sortedCssProperties += `${cssSelector} {\n ${cssProperties} \n}${
-         index === arrCssProperties.length - 1 ? "\n" : "\n\n"
-      }`;
+   // multiple sort (can sort css properties more than one css selector with css properties)
+   if (arrCssProperties[arrCssProperties.length - 1] === "") {
+      arrCssProperties.pop();
+
+      for (let index = 0; index < arrCssProperties.length; index++) {
+         const cssNode: string[] = arrCssProperties[index].split("{");
+         const cssSelector: string = cssNode[0].trim();
+
+         const cssProperties: string = cssNode[1]
+            .trim()
+            .split(";")
+            .filter((property) => property.trim() !== "")
+            .map((property) => property.trim() + ";")
+            .sort((a, b) =>
+               format === "min" ? a.length - b.length : b.length - a.length
+            )
+            .join("\n");
+
+         sortedCssProperties += `${cssSelector} {\n ${cssProperties} \n}${
+            index === arrCssProperties.length - 1 ? "\n" : "\n\n"
+         }`;
+      }
    }
 
    return sortedCssProperties;
