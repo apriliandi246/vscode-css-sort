6acf7b7036c77e271a2734961195985ea00e5dda
Farhan Apriliandi <apriliandi642@gmail.com>
2021-10-18 14:00:20 +0700

    refactor code and delete unused file and modules

--- src/extension.ts
+++ src/extension.ts
@@ -2,94 +2,106 @@ import * as vscode from "vscode";
 import { parse, generate } from "css-tree";
 
 export function activate(context: vscode.ExtensionContext) {
-   const minSort = vscode.commands.registerCommand("css-sort.min", () => {
-      const editor = vscode.window.activeTextEditor;
-      const cssCode = editor?.document.getText(editor.selection).trim();
+  const cssMinSort = vscode.commands.registerCommand("css-sort.min", () => {
+    const editor = vscode.window.activeTextEditor;
+    const cssCodeSelected = editor?.document.getText(editor.selection).trim();
 
-      if (!editor) {
-         vscode.window.showInformationMessage("Editor does not exist");
-         return;
-      }
+    if (!editor) {
+      vscode.window.showInformationMessage("Editor does not exist");
+      return;
+    }
 
-      if (!cssCode) {
-         vscode.window.showWarningMessage("No CSS code selected");
-         return;
-      }
+    if (!cssCodeSelected) {
+      vscode.window.showWarningMessage("No CSS code selected");
+      return;
+    }
 
-      editor.edit((oldText) => {
-         oldText.replace(editor.selection, onSort("min", cssCode));
-      });
+    editor.edit((currentCssCode) => {
+      currentCssCode.replace(
+        editor.selection,
+        sortCssProperties("min", cssCodeSelected)
+      );
+    });
 
-      editor.document.save();
-   });
+    editor.document.save();
+  });
 
-   const maxSort = vscode.commands.registerCommand("css-sort.max", () => {
-      const editor = vscode.window.activeTextEditor;
-      const cssCode = editor?.document.getText(editor.selection).trim();
+  const cssMaxSort = vscode.commands.registerCommand("css-sort.max", () => {
+    const editor = vscode.window.activeTextEditor;
+    const cssCodeSelected = editor?.document.getText(editor.selection).trim();
 
-      if (!editor) {
-         vscode.window.showInformationMessage("Editor does not exist");
-         return;
-      }
+    if (!editor) {
+      vscode.window.showInformationMessage("Editor does not exist");
+      return;
+    }
 
-      if (!cssCode) {
-         vscode.window.showWarningMessage("No CSS code selected");
-         return;
-      }
+    if (!cssCodeSelected) {
+      vscode.window.showWarningMessage("No CSS code selected");
+      return;
+    }
 
-      editor.edit((oldText) => {
-         oldText.replace(editor.selection, onSort("max", cssCode));
-      });
+    editor.edit((currentCssCode) => {
+      currentCssCode.replace(
+        editor.selection,
+        sortCssProperties("max", cssCodeSelected)
+      );
+    });
 
-      editor.document.save();
-   });
+    editor.document.save();
+  });
 
-   context.subscriptions.push(minSort, maxSort);
+  context.subscriptions.push(cssMinSort, cssMaxSort);
 }
 
-function onSort(pattern: string, cssCode: string): string {
-   let sortedCssProperties: string = "";
-   const cssAst = parse(cssCode, { parseValue: false });
-   const arrCssProperties: string[] = generate(cssAst).split("}");
+function sortCssProperties(pattern: string, cssCode: string): string {
+  let sortedCssProperties: string = "";
+  const cssAst = parse(cssCode, { parseValue: false });
+  const arrCssProperties: string[] = generate(cssAst).split("}");
 
-   // single sort (just css properties without css selector)
-   if (arrCssProperties[arrCssProperties.length - 1] !== "") {
-      sortedCssProperties = cssCode
-         .trim()
-         .split("\n")
-         .filter((property) => property.trim() !== "")
-         .map((property) => property.trim() + ";")
-         .sort((a, b) =>
-            pattern === "min" ? a.length - b.length : b.length - a.length
-         )
-         .join("\n");
-   }
+  // Single sort
+  // Just css properties without css selector
+  if (arrCssProperties[arrCssProperties.length - 1] !== "") {
+    sortedCssProperties = cssCode
+      .trim()
+      .split("\n")
+      .filter((property) => property.trim() !== "")
+      .map((property) => property.trim() + ";")
+      .sort((firstProperty, secondProperty) =>
+        pattern === "min"
+          ? firstProperty.length - secondProperty.length
+          : secondProperty.length - firstProperty.length
+      )
+      .join("\n");
+  }
 
-   // multiple sort (can sort css properties more than one css selector with css properties)
-   if (arrCssProperties[arrCssProperties.length - 1] === "") {
-      arrCssProperties.pop();
+  // Multiple sort
+  // Can sort css properties more than one css selector with css properties
+  if (arrCssProperties[arrCssProperties.length - 1] === "") {
+    arrCssProperties.pop();
 
-      for (let index = 0; index < arrCssProperties.length; index++) {
-         const cssNode: string[] = arrCssProperties[index].split("{");
-         const cssSelector: string = cssNode[0].trim();
+    for (let index = 0; index < arrCssProperties.length; index++) {
+      const cssNode: string[] = arrCssProperties[index].split("{");
+      const cssSelector: string = cssNode[0].trim();
 
-         const cssProperties: string = cssNode[1]
-            .trim()
-            .split(";")
-            .filter((property) => property.trim() !== "")
-            .map((property) => property.trim() + ";")
-            .sort((a, b) =>
-               pattern === "min" ? a.length - b.length : b.length - a.length
-            )
-            .join("\n");
+      const cssProperties: string = cssNode[1]
+        .trim()
+        .split(";")
+        .filter((property) => property.trim() !== "")
+        .map((property) => property.trim() + ";")
+        .sort((firstProperty, secondProperty) =>
+          pattern === "min"
+            ? firstProperty.length - secondProperty.length
+            : secondProperty.length - firstProperty.length
+        )
+        .join("\n");
 
-         sortedCssProperties += `${cssSelector} {\n ${cssProperties} \n}${
-            index === arrCssProperties.length - 1 ? "\n" : "\n\n"
-         }`;
-      }
-   }
+      sortedCssProperties += `${cssSelector} {\n ${cssProperties} \n}${
+        index === arrCssProperties.length - 1 ? "\n" : "\n\n"
+      }`;
+    }
+  }
 
-   return sortedCssProperties;
+  return sortedCssProperties;
 }
 
 export function deactivate() {}
